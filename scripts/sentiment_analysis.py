9684d238771d9cfd09d1d5950a65c45bfc997737
habtam440@gmail.com <habtam440@gmail.com>
2025-06-10 10:58:59 +0300

    fix syntax error

--- scripts/sentiment_analysis.py
+++ scripts/sentiment_analysis.py
@@ -37,9 +37,9 @@ def compute_sentiment(df, method='textblob'):
             analysis = TextBlob(str(text))
             polarity = analysis.sentiment.polarity
             
-            if polarity >=4.0 :
+            if polarity >= 4.0 :
                 label = 'POSITIVE'
-            elif polarity <=2.0 :
+            elif polarity <= 2.0 :
                 label = 'NEGATIVE'
             else:
                 label = 'NEUTRAL'
